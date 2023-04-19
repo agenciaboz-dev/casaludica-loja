defc6390f42daf7fc2e81073ffc72fa0298bccf3
nandobfer <nandobfer@yahoo.com.br>
2023-04-19 12:23:40 -0300

    feat: added muiTheme

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import './sass/_all.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { ThemeProvider } from "@mui/material";
+import { useMuiTheme } from './hooks/useMuiTheme';
 
 function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route index element={<Home />} />
-        </Routes>
-    </BrowserRouter>
+    const muiTheme = useMuiTheme()
+
+    return (
+        <ThemeProvider theme={muiTheme}>
+            
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Home />} />
+                </Routes>
+
+            </BrowserRouter>
+
+        </ThemeProvider>
   )
 }
 
