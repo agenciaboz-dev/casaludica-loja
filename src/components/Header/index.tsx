2e3ff81ce0344c804bdf637931d7efc7f8101c98
nandobfer <nandobfer@yahoo.com.br>
2023-04-20 10:06:13 -0300

    feat: added logo to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,20 +3,27 @@ import './style.scss';
 import MenuIcon from '@mui/icons-material/Menu';
 import { IconButton } from '@mui/material';
 import { Menu } from '../Menu'
+import { ReactComponent as CartIcon } from '../../images/cart.svg'
+import { ReactComponent as LogoIcon } from '../../images/logo.svg'
 
 export const Header = () => {
 
     const [openMenu, setOpenMenu] = useState(false)
+    const [openCart, setOpenCart] = useState(false)
     
     return (
         <>
             <div className='Header-Component' >
                 <IconButton color='secondary' sx={{gap: '2vw'}} onClick={() => setOpenMenu(true)}>
                     <MenuIcon sx={{width: '13vw', height: 'auto'}} />
-                    <p>Menu</p>
+                </IconButton>
+                <LogoIcon />
+                <IconButton color='secondary' sx={{gap: '2vw'}} onClick={() => setOpenCart(true)}>
+                    <CartIcon />
                 </IconButton>
             </div>
             <Menu isOpen={openMenu} setOpen={setOpenMenu} />
+
         </>
     )
 }
