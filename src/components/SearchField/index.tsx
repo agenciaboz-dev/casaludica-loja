50cb14d36249b9bc2a72d2d1f3d0f3d13c9146e7
nandobfer <nandobfer@yahoo.com.br>
2023-04-24 10:51:17 -0300

    update: searchfield underline and margin

--- src/components/SearchField/index.tsx
+++ src/components/SearchField/index.tsx
@@ -13,15 +13,17 @@ export const SearchField:React.FC<SearchFieldProps> = ({  }) => {
         <div className='SearchField-Component' >
             <TextField
                 id="input-with-icon-textfield"
-                sx={{backgroundColor: 'white'}}
+                sx={{backgroundColor: 'white', borderRadius: '2vw'}}
                 InputProps={{
                     startAdornment: (
                         <InputAdornment position="start" sx={{marginTop: '0 !important'}}>
                             <SearchIcon sx={{opacity: 0.5, width: '7vw', height: 'auto'}} />
                         </InputAdornment>
                     ),
+                    disableUnderline: true,
+                    sx:{borderRadius: '2vw !important'}
                 }}
-                inputProps={{sx: {padding: '3vw 0'}}}
+                inputProps={{sx: {padding: '3vw 0', borderRadius: '2vw'}}}
                 variant="filled"
             />
         </div>
