94189faf74f7c6416f61831ef6be025317a62944
nandobfer <nandobfer@yahoo.com.br>
2023-04-25 09:26:35 -0300

    update: promotion image

--- src/hooks/usePromotions.ts
+++ src/hooks/usePromotions.ts
@@ -4,13 +4,13 @@ export const usePromotions = () => {
     const promotions:Promotion[] = [
         {
             id: 1,
-            image_url: 'https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055_1280.jpg',
+            image_url: '/1.png',
             subtitle: 'Até 5x sem juros'
         },
         {
             id: 2,
-            image_url: 'https://static.vecteezy.com/ti/fotos-gratis/p3/6671766-fantastica-lua-magica-luz-e-agua-barco-com-arvore-papel-de-parede-gratis-foto.jpg',
-            subtitle: 'Até 5x sem juros'
+            image_url: 'https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055_1280.jpg',
+            subtitle: 'Até 8x sem juros'
         },
     ]
 
