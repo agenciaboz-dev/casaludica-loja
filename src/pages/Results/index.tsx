9a21e018e296e5ed5cf5cb43765d2e6f50ec93b0
nandobfer <nandobfer@yahoo.com.br>
2023-04-26 16:25:13 -0300

    update: added footer to results

--- src/pages/Results/index.tsx
+++ src/pages/Results/index.tsx
@@ -2,39 +2,43 @@ import { Avatar, Button } from '@mui/material';
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { Background } from '../../components/Background';
-import { Header } from '../../components/Header';
-import { SearchField } from '../../components/SearchField';
-import { Product } from '../../definitions/products';
-import { useCart } from '../../hooks/useCart';
-import { Collections } from '../Home/Collections';
-import './style.scss';
+import { Footer } from '../../components/Footer'
+import { Header } from '../../components/Header'
+import { SearchField } from '../../components/SearchField'
+import { Product } from '../../definitions/products'
+import { useCart } from '../../hooks/useCart'
+import { Collections } from '../Home/Collections'
+import './style.scss'
 
-interface ResultsProps {
-    
-}
+interface ResultsProps {}
 
-export const Results:React.FC<ResultsProps> = ({  }) => {
-    const location = useLocation()
-    const products = location.state.products || []
-    const cart = useCart()
-    
-    return (
-        <div className='Results-Page' >
-            <Background />
-            <Header />
+export const Results: React.FC<ResultsProps> = ({}) => {
+	const location = useLocation()
+	const products = location.state.products || []
+	const cart = useCart()
 
-            <SearchField />
-            <Collections />
-            
-            {products.map((product:Product) => 
-            <div style={{flexDirection: 'column', alignItems: 'center'}} key={product.id}>
-                <h1>{product.name}</h1>
-                <Avatar src={product.cover} sx={{width: '50vw', height: 'auto'}} />
-                <h3>{product.resume}</h3>
-                <p>{product.description}</p>
-                <Button variant='contained' onClick={() => cart.add(product)} >Eu quero</Button>
-            </div>)}
-            {products.length == 0 && <p style={{alignSelf: 'center'}}>Nenhum resultado</p>}
-        </div>
-    )
+	return (
+		<div className="Results-Page">
+			<Background />
+			<Header />
+
+			<SearchField />
+			<Collections />
+
+			{products.map((product: Product) => (
+				<div style={{ flexDirection: 'column', alignItems: 'center' }} key={product.id}>
+					<h1>{product.name}</h1>
+					<Avatar src={product.cover} sx={{ width: '50vw', height: 'auto' }} />
+					<h3>{product.resume}</h3>
+					<p>{product.description}</p>
+					<Button variant="contained" onClick={() => cart.add(product)}>
+						Eu quero
+					</Button>
+				</div>
+			))}
+			{products.length == 0 && <p style={{ alignSelf: 'center' }}>Nenhum resultado</p>}
+
+			<Footer />
+		</div>
+	)
 }
