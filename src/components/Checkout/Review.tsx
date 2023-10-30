66fc09152e7423d1a2aec6b4a83ecef93d3db90d
nandobfer <nandobfer@yahoo.com.br>
2023-10-30 17:22:06 -0300

    update: review title

--- src/components/Checkout/Review.tsx
+++ src/components/Checkout/Review.tsx
@@ -9,45 +9,46 @@ export const Review: React.FC<ReviewProps> = ({}) => {
     const cart = useCart()
 
     return (
-        <Paper elevation={5} sx={{ flexDirection: "column", borderRadius: "4.5vw", gap: "5vw", padding: "3vw", color: "#686868" }}>
-            <Grid container spacing={1.5}>
-                <Grid item xs={6}>
-                    <p style={{ fontWeight: "bold" }}>Produtos</p>
-                </Grid>
-                <Grid item xs={6}>
-                    <p style={{ fontWeight: "bold" }}>Subtotal</p>
-                </Grid>
-            </Grid>
-
-            {cart.products.map((product) => (
-                <Grid container spacing={1.5} key={product.id}>
-                    <Grid item xs={6}>
-                        <p>{product.name}</p>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <CurrencyText value={product.price * product.quantity} />
-                    </Grid>
-                </Grid>
-            ))}
-
-            <Grid container spacing={1.5}>
-                <Grid item xs={6}>
-                    <p style={{ fontWeight: "bold" }}>Subtotal</p>
-                </Grid>
-                <Grid item xs={6}>
-                    <CurrencyText value={cart.total} style={{ fontWeight: "bold" }} />
-                </Grid>
-            </Grid>
-            <Box sx={{ borderTop: "1px solid", borderColor: "primary.main", color: "black", paddingTop: "5vw" }}>
+        <Box sx={{ flexDirection: "column", color: "primary.main", gap: "5vw" }}>
+            <p style={{ fontFamily: "BowlbyOneSC" }}>Seu pedido</p>
+            <Paper elevation={5} sx={{ flexDirection: "column", borderRadius: "4.5vw", gap: "5vw", padding: "3vw", color: "#686868" }}>
                 <Grid container spacing={1.5}>
                     <Grid item xs={6}>
-                        <p style={{ fontWeight: "bold" }}>Total</p>
+                        <p style={{ fontWeight: "bold" }}>Produtos</p>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <p style={{ fontWeight: "bold" }}>Subtotal</p>
+                    </Grid>
+                </Grid>
+                {cart.products.map((product) => (
+                    <Grid container spacing={1.5} key={product.id}>
+                        <Grid item xs={6}>
+                            <p>{product.name}</p>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <CurrencyText value={product.price * product.quantity} />
+                        </Grid>
+                    </Grid>
+                ))}
+                <Grid container spacing={1.5}>
+                    <Grid item xs={6}>
+                        <p style={{ fontWeight: "bold" }}>Subtotal</p>
                     </Grid>
                     <Grid item xs={6}>
                         <CurrencyText value={cart.total} style={{ fontWeight: "bold" }} />
                     </Grid>
                 </Grid>
-            </Box>
-        </Paper>
+                <Box sx={{ borderTop: "1px solid", borderColor: "primary.main", color: "black", paddingTop: "5vw" }}>
+                    <Grid container spacing={1.5}>
+                        <Grid item xs={6}>
+                            <p style={{ fontWeight: "bold" }}>Total</p>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <CurrencyText value={cart.total} style={{ fontWeight: "bold" }} />
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Paper>
+        </Box>
     )
 }
