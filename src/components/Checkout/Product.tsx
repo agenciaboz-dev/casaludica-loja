d5de2f89a91012146fc2392234c36fae00f9547e
tainarasn <tainarasn.sba@gmail.com>
2023-11-06 12:44:08 -0300

    fix: refactoring product checkout

--- src/components/Checkout/Product.tsx
+++ src/components/Checkout/Product.tsx
@@ -23,30 +23,36 @@ export const Product: React.FC<ProductProps> = ({ product }) => {
         }
     }
     return (
-        <Box className="product-container" sx={{ position: "relative", justifyContent: "center", alignItems: "center", gap: "3vw" }}>
-            <img src={"data:image/jpeg;base64," + product.cover} alt="product" className="img-product" style={{ width: "37vw", height: "37vw" }} />
-            <Box className="right-container" sx={{ color: "#363775", fontWeight: "500", fontSize: "3vw", flexDirection: "column" }}>
-                <p className="name-product">
-                    Produto: <span style={{ fontWeight: "bold" }}>{product.name}</span>
-                </p>
-                <p className="quantity">
-                    Quantidade:{" "}
-                    <input
-                        type="number"
-                        className="number"
-                        value={productQuantity}
-                        onChange={(event) => setProductQuantity(Number(event.target.value))}
-                        style={{ width: "30%", borderRadius: "1.6vw", height: "5.5vw" }}
-                    />
-                </p>
-                <p className="price-product">
+        <Box sx={{ position: "relative", justifyContent: "center", alignItems: "center", gap: "3vw" }}>
+            <img src={"data:image/jpeg;base64," + product.cover} alt="product" style={{ width: "37vw", height: "37vw" }} />
+            <Box sx={{ color: "#363775", fontWeight: "500", fontSize: "3vw", flexDirection: "column" }}>
+                <Box sx={{ flexDirection: "column" }}>
+                    <p style={{ fontSize: "3.1vw" }}>
+                        Produto: <span style={{ fontWeight: "bold" }}>{product.name}</span>
+                    </p>
+                    <Box sx={{ flexDirection: "row", gap: "1vw", alignItems: "center" }}>
+                        <p style={{ fontSize: "3.1vw" }}>Quantidade: </p>
+                        <input
+                            type="number"
+                            className="number"
+                            value={productQuantity}
+                            onChange={(event) => setProductQuantity(Number(event.target.value))}
+                            style={{ width: "30%", borderRadius: "1.6vw", height: "5.5vw", padding: "2vw" }}
+                        />
+                    </Box>
+                </Box>
+                <p style={{ fontSize: "3.5vw" }}>
                     Preço:{" "}
-                    <span className="price" style={{ color: "#686868", fontWeight: "bolder", fontSize: "3vw" }}>
-                        R${product.price}
+                    <span style={{ color: "#686868", fontWeight: "bolder", fontSize: "3.8vw" }}>
+                        R${product.price * productQuantity}
                     </span>
                 </p>
             </Box>
-            <IconButton className="close" onClick={deleteProduct} sx={{ position: "absolute", top: "0vw", right: "0vw" }}>
+            <IconButton
+                className="close"
+                onClick={deleteProduct}
+                sx={{ position: "absolute", top: "0vw", right: "0vw", padding: 0 }}
+            >
                 {/* <CloseIcon style={{ height: "auto", width: "6vw" }} /> */}x
             </IconButton>
         </Box>
--- src/components/Checkout/index.tsx
+++ src/components/Checkout/index.tsx
@@ -59,13 +59,19 @@ export const Checkout: React.FC<CheckoutProps> = ({}) => {
     }
 
     return (
-        <Box className="Checkout-Component" sx={{ flexDirection: "column", width: "100%", padding: "0 5vw", gap: "5vw", paddingBottom: "5vw" }}>
+        <Box
+            className="Checkout-Component"
+            sx={{ flexDirection: "column", width: "100%", padding: "0 5vw", gap: "5vw", paddingBottom: "5vw" }}
+        >
             <Background />
             <Header />
             <SearchField />
             <Collections />
 
-            <p className="finish" style={{ fontWeight: "bold", fontSize: "5vw", color: "#363775", fontFamily: "BowlbyOneSC" }}>
+            <p
+                className="finish"
+                style={{ fontWeight: "bold", fontSize: "5vw", color: "#363775", fontFamily: "BowlbyOneSC" }}
+            >
                 Finalizar Pedido
             </p>
             <Box
@@ -74,10 +80,11 @@ export const Checkout: React.FC<CheckoutProps> = ({}) => {
                     width: "100%",
                     boxShadow: " 0 0 8px rgba(0,0,0,0.4)",
                     padding: "5vw",
-                    paddingBottom: "2vw",
+                    paddingBottom: "4vw",
                     borderRadius: "4.5vw",
                     flexDirection: "column",
                     height: "fit-content",
+
                     gap: "3vw",
                 }}
             >
