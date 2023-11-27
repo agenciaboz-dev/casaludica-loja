11c383d3ae3919bf75e2ee282189ee178a309b2f
ispadilha <israel.padilha@gmail.com>
2023-11-27 11:15:53 -0300

    style: footer copyright from mobile to desktop

--- src/components/Footer/Copyright/index.tsx
+++ src/components/Footer/Copyright/index.tsx
@@ -1,22 +1,24 @@
-import { Box } from '@mui/material';
-import React from 'react';
-import { useColors } from '../../../hooks/useColors';
+import { Box, useMediaQuery } from "@mui/material"
+import React from "react"
+import { useColors } from "../../../hooks/useColors"
 
-interface CopyrightProps {
-    
-}
+interface CopyrightProps {}
 
-export const Copyright:React.FC<CopyrightProps> = ({  }) => {
+export const Copyright: React.FC<CopyrightProps> = ({}) => {
+    const isMobile = useMediaQuery("(orientation: portrait)")
     const colors = useColors()
-    
+
     return (
-        <Box className='Copyright-Component' style={{
-            margin: "10vw 0",
-            width: "100%",
-            flexDirection: "column",
-            textAlign: "center",
-            color: colors.primary
-        }}>
+        <Box
+            className="Copyright-Component"
+            style={{
+                margin: isMobile ? "10vw 0" : "0 0 5vw",
+                width: "100%",
+                flexDirection: "column",
+                textAlign: "center",
+                color: colors.primary,
+            }}
+        >
             <p>2023 © Direitos Reservados</p>
             <p>Powered By BOZ</p>
         </Box>
