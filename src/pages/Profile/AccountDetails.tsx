6eb8cb254979a0d5dda3906eb1e46b2d2979966d
nandobfer <nandobfer@yahoo.com.br>
2024-03-11 14:11:33 -0300

    feat: updating profile data

--- src/pages/Profile/AccountDetails.tsx
+++ src/pages/Profile/AccountDetails.tsx
@@ -1,19 +1,45 @@
-import React from "react"
-import { Box, Button } from "@mui/material"
+import React, { useState } from "react"
+import { Box, Button, CircularProgress } from "@mui/material"
 import { CollapsibleMenu } from "./CollapsibleMenu"
 import { RoundedTextField } from "../../components/RoundedTextField"
 import { useFormik } from "formik"
+import { api } from "../../api"
+import { useUser } from "../../hooks/useUser"
+import { useSnackbar } from "burgos-snackbar"
 
 interface AccountDetailsProps {
     user: User
 }
 
 export const AccountDetails: React.FC<AccountDetailsProps> = ({ user }) => {
+    const { setUser } = useUser()
+    const { snackbar } = useSnackbar()
+
+    const [loading, setLoading] = useState(false)
+
     const formik = useFormik({
-        initialValues: user,
-        onSubmit: (values) => {
+        initialValues: {
+            id: user.id,
+            name: user.name,
+            lastname: user.lastname,
+            email: user.email,
+            phone: user.phone,
+        },
+        onSubmit: async (values) => {
+            if (loading) return
+
+            setLoading(true)
             console.log(values)
-        }
+            try {
+                const response = await api.post("/user/update", values)
+                setUser(response.data)
+                snackbar({ severity: "info", text: "dados atualizados" })
+            } catch (error) {
+                console.log(error)
+                snackbar({ severity: "error", text: "erro ao atualizar dados, cheque o console" })
+            }
+            setLoading(false)
+        },
     })
 
     return (
@@ -25,7 +51,7 @@ export const AccountDetails: React.FC<AccountDetailsProps> = ({ user }) => {
                     <RoundedTextField label="E-mail" value={formik.values.email} name="email" onChange={formik.handleChange} />
                     <RoundedTextField label="Telefone" value={formik.values.phone} name="phone" onChange={formik.handleChange} />
                     <Button type="submit" variant="contained" sx={{ alignSelf: "flex-end", borderRadius: "10vw" }}>
-                        Salvar
+                        {loading ? <CircularProgress size="1.5rem" color="secondary" /> : "salvar"}
                     </Button>
                 </Box>
             </form>
