e4aa64b19f13f3f6ca3b08e8bee3f6f40e814d7c
nandobfer <nandobfer@yahoo.com.br>
2024-03-25 17:59:17 -0300

    update: franchise

--- src/types/server/class/Franchise.d.ts
+++ src/types/server/class/Franchise.d.ts
@@ -4,6 +4,8 @@ export declare class Franchise {
     name: string;
     fantasy_name: string;
     cnpj: string;
+    email: string;
+    phone: string;
     address: {
         street: string;
         number: string;
