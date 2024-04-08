8da88b87337f344fa90dad6528ce2ef794d60adf
nandobfer <nandobfer@yahoo.com.br>
2024-04-08 16:49:24 -0300

    update: zapzap

--- src/components/Zap.tsx
+++ src/components/Zap.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 import { Box, IconButton, useMediaQuery } from "@mui/material"
 import { useFranchise } from "../hooks/useFranchise"
-import { WhatsApp } from "@mui/icons-material"
 import unmask from "../tools/unmask"
+import image from "../images/whatsapp_alt.svg"
 
 interface ZapProps {}
 
@@ -12,11 +12,11 @@ export const Zap: React.FC<ZapProps> = ({}) => {
 
     const url = `https://wa.me/55${unmask(franchise?.phone || "")}`
     const offset = isMobile ? 5 : 10
-    const size = isMobile ? "15vw" : "4vw"
+    const size = isMobile ? "14vw" : "4vw"
 
     return (
         <IconButton sx={{ position: "fixed", bottom: offset, right: offset }} color="success" onClick={() => window.open(url, "_blank")?.focus()}>
-            <WhatsApp sx={{ width: size, height: size }} />
+            <img src={image} style={{ width: size, height: size }} />
         </IconButton>
     )
 }
