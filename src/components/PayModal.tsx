d1fd812e5d7838796a57485117e1d88e27dfb853
nandobfer <nandobfer@yahoo.com.br>
2024-04-10 15:23:00 -0300

    update: changed bozpay to our token

--- src/components/PayModal.tsx
+++ src/components/PayModal.tsx
@@ -42,26 +42,26 @@ export const PayModal: React.FC<PayModalProps> = ({ open, orderId, close }) => {
         >
             <Box sx={{ height: "fit-content", flexDirection: "column", alignItems: "center" }}>
                 {!!orderId && (
-                    <BozPay
-                        pagseguroToken={franchise.pagseguro_token}
-                        pagseguroTokenSandbox={franchise.pagseguro_token_sandbox}
-                        creditCardPublicKey={franchise.credit_card_public_key}
-                        storeIdentifier={bozpayStoreIdentifier}
-                        referenceId={orderId}
-                        sandbox
-                        wrapperSx={{ width: "100%", alignSelf: "center" }}
-                        onPaid={onPaid}
-                    />
                     // <BozPay
-                    //     pagseguroToken="5e137c4a-acd6-433a-83a7-736815c6995b0ad8f02a47329494fac489b021d5ab384b54-9b9f-4140-b4cf-4675e700a829"
-                    //     pagseguroTokenSandbox="1BD9D2D2181B4660BAFC9426CA5A63A9"
-                    //     creditCardPublicKey="MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApv7yDaw4aK+JNxjhxH7P1eTxZvoMfL2S4RifLUUB0+KBlN6uvKVj40wiBhLH7O9EPZeoVdApoK0M78Kol9LT3LYU4jQ0dFeeTeD/NV3AUguVBdJdIu8cUs0+oVvWAB4e0niEgax480x/Go7XG1ffvYAaYkO5FTeEH4qrwbz13a4ALPZ93ge6c6xZVspzAZc+WVnxcLeeoMoD4xz8DZS2LbqwOF9ee8Pcb8ybdr8p0vJL056Kb8AKYZ1mZ88nsdIqmR1jZ+BqRH6zMHW6UCVX6NgqWPkwemsfr2R5S+1EnHGH7ZAiUtRzCpejGVV33PIgOlb7j4JcCvQ6YP07AiiF3QIDAQAB"
+                    //     pagseguroToken={franchise.pagseguro_token}
+                    //     pagseguroTokenSandbox={franchise.pagseguro_token_sandbox}
+                    //     creditCardPublicKey={franchise.credit_card_public_key}
                     //     storeIdentifier={bozpayStoreIdentifier}
                     //     referenceId={orderId}
                     //     sandbox
                     //     wrapperSx={{ width: "100%", alignSelf: "center" }}
                     //     onPaid={onPaid}
                     // />
+                    <BozPay
+                        pagseguroToken="5e137c4a-acd6-433a-83a7-736815c6995b0ad8f02a47329494fac489b021d5ab384b54-9b9f-4140-b4cf-4675e700a829"
+                        pagseguroTokenSandbox="1BD9D2D2181B4660BAFC9426CA5A63A9"
+                        creditCardPublicKey="MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApv7yDaw4aK+JNxjhxH7P1eTxZvoMfL2S4RifLUUB0+KBlN6uvKVj40wiBhLH7O9EPZeoVdApoK0M78Kol9LT3LYU4jQ0dFeeTeD/NV3AUguVBdJdIu8cUs0+oVvWAB4e0niEgax480x/Go7XG1ffvYAaYkO5FTeEH4qrwbz13a4ALPZ93ge6c6xZVspzAZc+WVnxcLeeoMoD4xz8DZS2LbqwOF9ee8Pcb8ybdr8p0vJL056Kb8AKYZ1mZ88nsdIqmR1jZ+BqRH6zMHW6UCVX6NgqWPkwemsfr2R5S+1EnHGH7ZAiUtRzCpejGVV33PIgOlb7j4JcCvQ6YP07AiiF3QIDAQAB"
+                        storeIdentifier={bozpayStoreIdentifier}
+                        referenceId={orderId}
+                        sandbox
+                        wrapperSx={{ width: "100%", alignSelf: "center" }}
+                        onPaid={onPaid}
+                    />
                 )}
             </Box>
             <Dialog open={orderButton} hideBackdrop PaperProps={{ sx: { display: "contents" } }} sx={{ justifyContent: "center" }}>
